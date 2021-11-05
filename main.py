837f026bba34095b22f838fff1f0444b83f05f93
MohNabilAwad <79405221+MohNabilAwad@users.noreply.github.com>
2021-11-05 17:43:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -50,14 +50,14 @@ def StoreCurrentPrices():
     ftp.storlines('STOR ' + filename, open(filename, 'rb'))
     ftp.quit()
     
-    @app.route("/")
-    def hello():
-        return DateTime
+    #@app.route("/")
+    #def hello():
+    #    return DateTime
     
-    if __name__ == '__main__':
+    #if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-        port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port)
+    #    port = int(os.environ.get('PORT', 5000))
+    #    app.run(host='0.0.0.0', port=port)
 
 
     
